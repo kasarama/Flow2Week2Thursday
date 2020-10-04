a2140ca0f36ef7f85b485c834c781e5574d24aa7
kasarama <magdalena.wawrzak@gmail.com>
2020-10-04 12:17:33 +0200

    cors updated, dependency changed

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -23,7 +23,7 @@ public class ApplicationConfig extends Application {
         resources.add(exceptions.GenericExceptionMapper.class);
         resources.add(exceptions.MissingInputExceptionMapper.class);
         resources.add(exceptions.PersonNotFoundExceptionMapper.class);
-        resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+        resources.add(rest.CorsFilter.class);
         resources.add(rest.PersonResource.class);
         resources.add(rest.TestResource.class);
     }
